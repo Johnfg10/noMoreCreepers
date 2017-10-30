f4add69360f1e9cf37b9cda3307de0c0a07367d5
Johnfg10 <johnfg2610@gmail.com>
2017-10-30 20:09:51 +0000

    init

---
+++ src/main/java/io/github/johnfg10/nomorecreepers/NoMoreCreepers.java
@@ -0,0 +1,40 @@
+package io.github.johnfg10.nomorecreepers;
+
+import com.google.inject.Inject;
+import org.slf4j.Logger;
+import org.spongepowered.api.data.key.Keys;
+import org.spongepowered.api.data.value.mutable.OptionalValue;
+import org.spongepowered.api.entity.Entity;
+import org.spongepowered.api.entity.EntityTypes;
+import org.spongepowered.api.entity.living.monster.Creeper;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.entity.SpawnEntityEvent;
+import org.spongepowered.api.event.filter.Getter;
+import org.spongepowered.api.event.game.state.GameStartedServerEvent;
+import org.spongepowered.api.event.network.ClientConnectionEvent;
+import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.format.TextColors;
+import org.spongepowered.api.text.format.TextStyles;
+
+@Plugin(
+        id = "nomorecreepers",
+        name = "NoMoreCreepers",
+        version = "1.0.0",
+        description = "Simply disables creeper explosions"
+)
+public class NoMoreCreepers {
+    @Listener
+    public void onEntitySpawnEvent(SpawnEntityEvent event){
+        for (Entity entity : event.getEntities()) {
+            if (entity.getType().equals(EntityTypes.CREEPER)){
+                if (entity instanceof Creeper){
+                    System.out.println("creeper located");
+                    Creeper creeper = (Creeper) entity;
+                    creeper.explosionRadius().setTo(0);
+                }
+            }
+        }
+    }
+}
